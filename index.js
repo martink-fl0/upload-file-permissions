cd2519c8a95452a619db30f5a197b84d9b316953
martink-fl0 <144083279+martink-fl0@users.noreply.github.com>
2023-12-06 21:05:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const multipart = require("connect-multiparty");
 
+const fs = require("fs");
+
 const md_upload = multipart({ uploadDir: "./uploads" });
 
 const app = express();
@@ -9,6 +11,17 @@ const port = 8080;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const systemUser = process.env.USER;
+console.log(`System user: ${systemUser}`);
+
+fs.access(md_upload.uploadDir, (err) => {
+  if (err) {
+    console.error(`Error accessing ./uploads folder: ${err.message}`);
+  } else {
+    console.log(`Permissions for ./uploads folder: Readable and Writable`);
+  }
+});
 
 // Route
 app.post("/create", [md_upload], (req, res) => {
